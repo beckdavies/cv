d129dfe486fb60b63a4ff1693a09081eaef21883
Beck Davies <beck.davies@therealbuzzgroup.com>
2017-04-06 23:53:37 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css')
+    rename = require('gulp-rename');
+
+var paths = {
+      styles: {
+          src: 'src/scss/*.scss',
+          dest: 'built/css/'
+      }
+    };
+
+gulp.task('sass', function(){
+  return gulp.src(paths.styles.src)
+    .pipe(sass())
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest(paths.styles.dest))
+});
+
+
+// Gulp watch syntax
+gulp.task('watch', function(){
+  gulp.watch(paths.styles.src, ['sass']); 
+  // Other watchers
+})
