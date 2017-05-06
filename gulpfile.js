a42752d5dc791da21263415b20f9cd4674cd40bd
Beck Davies <beck.davies@therealbuzzgroup.com>
2017-05-06 23:24:10 +0100

    adding the js files to the repo, and the js tasks to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,63 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    cleanCSS = require('gulp-clean-css')
-    rename = require('gulp-rename');
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
 
 var paths = {
-      styles: {
+      sass: {
           src: 'src/scss/*.scss',
           dest: 'built/css/'
-      }
+      },
+      js: {
+          src: 'src/js/*.js',
+          dest: 'built/js/'
+      },
+
     };
 
+
+// ---- //
+// SASS //
+// ---- //
+
 gulp.task('sass', function(){
-  return gulp.src(paths.styles.src)
+  return gulp.src(paths.sass.src)
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(gulp.dest(paths.sass.dest))
     .pipe(rename({suffix: '.min'}))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(gulp.dest(paths.sass.dest))
+});
+
+// -- //
+// JS //
+// -- //
+
+gulp.task('js', function() {  
+    return gulp.src(paths.js.src)
+        .pipe(gulp.dest(paths.sass.dest))
+        .pipe(rename('scripts.min.js'))
+        //.pipe(uglify())
+        .pipe(uglify().on('error', function(e){
+            console.log(e);
+         }))
+        .pipe(gulp.dest(paths.js.dest));
 });
 
 
-// Gulp watch syntax
+// ----- //
+// WATCH //
+// ----- //
+
 gulp.task('watch', function(){
-  gulp.watch(paths.styles.src, ['sass']); 
+  gulp.watch(paths.sass.src, ['sass']);
+  gulp.watch(paths.js.src, ['js']); 
   // Other watchers
 })
+
---
+++ src/js/scripts.js
@@ -0,0 +1,3 @@
+// this is the comments
+
+var my_var;
